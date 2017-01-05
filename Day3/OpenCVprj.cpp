64baec2793588298275e883936b24f3773185a20
DoommeR <success-07@list.ru>
2017-01-05 20:45:00 +0300

    day3_openCV

---
+++ Day3/OpenCVprj.cpp
@@ -0,0 +1,52 @@
+﻿// OpenCVprj.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int, char**) {
+	VideoCapture cap("video.MOV"); // Открываем камеру или файл
+
+	if(!cap.isOpened())     // check if we succeeded
+		return -1;
+	
+	double fps = cap.get(CV_CAP_PROP_FPS);     //get the frames per seconds of the video 
+	std::cout << "Frame per seconds : " << fps << std::endl; 
+	namedWindow("MyVideo",CV_WINDOW_AUTOSIZE); //create a window called "MyVideo"   
+	
+	
+	int trd = 200;
+	std::vector<Vec4f> lines_std;
+	Ptr<LineSegmentDetector> ls = createLineSegmentDetector(LSD_REFINE_STD);
+	
+	while (1)
+	{
+		Mat frame;
+		//Mat edges;
+		Mat gray_img;
+		bool bSuccess = cap.read(frame); // read a new frame from video 
+		if (!bSuccess)
+		{
+			std::cout << "Cannot read the frame from video file" << std::endl;
+			break;
+		}
+		
+		
+		cvtColor(frame, gray_img, COLOR_BGR2GRAY);
+		//Canny(frame, frame, trd*0.4, trd, 3);
+		ls->detect(gray_img, lines_std);
+		ls->drawSegments(frame, lines_std);
+
+		resize(frame, frame, Size(800, 600));
+		imshow("MyVideo", frame); //show the frame in "MyVideo" window   
+		
+		if (waitKey(30) == 27)
+		{
+			break;
+		}
+	}
+}
